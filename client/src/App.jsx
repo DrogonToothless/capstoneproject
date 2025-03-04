686b0b7126a7c26b9ecca813a433763a1ffa0a4d
NathanTerry <nathan.terry4540@stu.mtec.edu>
2025-03-04 08:47:51 -0700

    Really basic log in screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,6 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState(null);
-
   useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
@@ -15,27 +12,34 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <h1>{!data ? "Loading..." : data}</h1>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Log In</h1>
+
+      <form>
+        <div>
+        <label for="username">Enter Username</label>
+        <hr></hr>
+        <input 
+        name="username"
+        id="username"
+        type="text"
+        placeholder="Username here..."></input>
+        </div>
+
+        <div>
+        <label for="password">Enter Password</label>
+        <hr></hr>
+        <input 
+        name="password"
+        id="password"
+        type="text"
+        placeholder="Password here..."></input>
+        </div>
+
+        <button
+          type = "submit"
+          value = "Submit"
+        >Log In</button>
+      </form>
     </>
   );
 }
