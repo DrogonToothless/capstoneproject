5960fe309bfb325a934ec88352b9cf91d1394833
NathanTerry <nathan.terry4540@stu.mtec.edu>
2025-03-12 09:35:46 -0600

    fix annoying error in console about a lack of <thead>, <tbody> or <tfoot> elements

--- client/src/Courses.jsx
+++ client/src/Courses.jsx
@@ -35,16 +35,24 @@ function CoursesList() {
 
       {/*Table will be filled out with javascript*/}
       <table>
-        <tr>
-          <th>String ID</th>
-          <th>Course Title</th>
-          <th>Description</th>
-          <th>Schedule</th>
-          <th>Classroom Number</th>
-          <th>Maximum Capacity</th>
-          <th>Credit Hours</th>
-          <th>Tuition Cost</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>String ID</th>
+            <th>Course Title</th>
+            <th>Description</th>
+            <th>Schedule</th>
+            <th>Classroom Number</th>
+            <th>Maximum Capacity</th>
+            <th>Credit Hours</th>
+            <th>Tuition Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+        <tfoot>
+          
+        </tfoot>
       </table>
     </>
   );
