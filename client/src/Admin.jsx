b4c3678e8fa311b1a5c86f9067b17af0ae4eee15
NathanTerry <nathan.terry4540@stu.mtec.edu>
2025-03-19 07:50:27 -0600

    old admin panel commit I forgot to add

--- client/src/Admin.jsx
+++ client/src/Admin.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import "./Misc.css";
 
 // images, it's okay to import images like this manually because this is a small project
 import loading_icon from './assets/load.png';
@@ -13,7 +14,9 @@ function AdminPanel() {
 
   return (
     <>
-
+        <h1>
+          Admin Panel
+        </h1>
     </>
   );
 }
